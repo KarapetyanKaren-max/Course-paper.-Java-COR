89f7fc4f6397b859d59f23d25df22d40d77c8358
Karen <karapetyankaren33933@gmail.com>
2025-06-10 23:55:46 +0400

    initial commit

---
+++ src/main/java/org_skypro/counter_service/CourseworkApplication.java
@@ -0,0 +1,13 @@
+package org_skypro.counter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org_skypro/counter_service/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org_skypro.counter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
